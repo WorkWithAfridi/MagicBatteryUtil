317da755587414dec276a97260e4fba642203d37
Khondakar Afridi <afridi.khondakar@gmail.com>
2026-06-10 12:46:28 +0600

    Initial Commit

---
+++ MagicBatteryUtil/MagicBatteryUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicBatteryUtilApp.swift
+//  MagicBatteryUtil
+//
+//  Created by Khondakar Afridi on 10/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MagicBatteryUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
